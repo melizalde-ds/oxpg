4182fcf10b92950f7a47e72d37fd38014877a242
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2026-02-07 01:36:22 -0700

    Refactor connection logic in Client struct and enhance DSN handling

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,5 @@
-use pyo3::{PyErr, PyResult, pyclass, pyfunction, pymethods};
+use pyo3::{PyErr, PyResult, pyclass, pyfunction};
+use tokio_postgres::{Client as PgClient, Connection, Socket, tls::NoTlsStream};
 
 #[pyclass]
 struct Client {
@@ -6,7 +7,8 @@ struct Client {
     port: u16,
     db: String,
     user: String,
-    dsn: String,
+    client: PgClient,
+    runtime: tokio::runtime::Runtime,
 }
 
 #[pyfunction]
@@ -19,47 +21,96 @@ fn connect(
     port: u16,
     db: String,
 ) -> PyResult<Client> {
-    let connection_string = match dsn {
-        Some(s) => s,
-        None => build_dsn(host, user, password, port, db)?,
-    };
-    todo!()
-}
-
-fn build_dsn(
-    host: Option<String>,
-    user: Option<String>,
-    password: Option<String>,
-    port: u16,
-    db: String,
-) -> PyResult<String> {
-    let host =
-        host.ok_or_else(|| PyErr::new::<pyo3::exceptions::PyValueError, _>("host is required"))?;
-    let user =
-        user.ok_or_else(|| PyErr::new::<pyo3::exceptions::PyValueError, _>("user is required"))?;
-    let password = password
-        .ok_or_else(|| PyErr::new::<pyo3::exceptions::PyValueError, _>("password is required"))?;
-
-    Ok(format!(
-        "postgresql://{}:{}@{}:{}/{}",
-        user, password, host, port, db
-    ))
-}
-
-#[pymethods]
-impl Client {
-    #[new]
-    #[pyo3(signature = (host, user, password, port=5432, db="postgres".to_string()))]
-    fn new(host: String, user: String, password: String, port: u16, db: String) -> Self {
-        Client {
-            host: host.clone(),
-            port,
-            db: db.clone(),
-            user: user.clone(),
-            dsn: format!(
-                "host={} port={} dbname={} user={} password={}",
-                host, port, db, user, password
-            ),
+    let (host, user, port, db, connection_string) = match dsn {
+        Some(s) => extract_host_from_dsn(s)?,
+        None => {
+            let host = host.ok_or_else(|| {
+                PyErr::new::<pyo3::exceptions::PyValueError, _>("host is required")
+            })?;
+            let user = user.ok_or_else(|| {
+                PyErr::new::<pyo3::exceptions::PyValueError, _>("user is required")
+            })?;
+            let password = password.ok_or_else(|| {
+                PyErr::new::<pyo3::exceptions::PyValueError, _>("password is required")
+            })?;
+            (
+                host.clone(),
+                user.clone(),
+                port,
+                db.clone(),
+                format!(
+                    "postgresql://{}:{}@{}:{}/{}",
+                    user, password, host, port, db
+                ),
+            )
         }
-    }
+    };
+    let runtime = tokio::runtime::Runtime::new().map_err(|e| {
+        PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!(
+            "Failed to create Tokio runtime: {}",
+            e
+        ))
+    })?;
+
+    let (client, connection) = runtime
+        .block_on(async {
+            tokio_postgres::connect(&connection_string, tokio_postgres::NoTls).await
+        })
+        .map_err(|e| {
+            PyErr::new::<pyo3::exceptions::PyConnectionError, _>(format!(
+                "Failed to connect to database: {}",
+                e
+            ))
+        })?;
+
+    runtime.spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("Connection error: {}", e);
+        }
+    });
+
+    Ok(Client {
+        host,
+        port,
+        db,
+        user,
+        client,
+        runtime,
+    })
+}
+
+fn extract_host_from_dsn(dsn: String) -> PyResult<(String, String, u16, String, String)> {
+    let without_scheme = dsn
+        .strip_prefix("postgres://")
+        .or_else(|| dsn.strip_prefix("postgresql://"))
+        .ok_or_else(|| {
+            PyErr::new::<pyo3::exceptions::PyValueError, _>(
+                "Invalid DSN: must start with postgres:// or postgresql://",
+            )
+        })?;
+    let (auth, rest) = without_scheme.split_once('@').ok_or_else(|| {
+        PyErr::new::<pyo3::exceptions::PyValueError, _>(
+            "Invalid DSN: missing @ separating auth and host",
+        )
+    })?;
+    let (user, _) = auth.split_once(':').ok_or_else(|| {
+        PyErr::new::<pyo3::exceptions::PyValueError, _>("Invalid DSN: auth must be user:password")
+    })?;
+    let (host_port, db) = rest.split_once('/').ok_or_else(|| {
+        PyErr::new::<pyo3::exceptions::PyValueError, _>(
+            "Invalid DSN: missing database name after /",
+        )
+    })?;
+    let (host, port) = match host_port.split_once(':') {
+        Some((h, p)) => {
+            let port = p.parse::<u16>().map_err(|_| {
+                PyErr::new::<pyo3::exceptions::PyValueError, _>(
+                    "Invalid DSN: port must be a number",
+                )
+            })?;
+            (h.to_string(), port)
+        }
+        None => (host_port.to_string(), 5432),
+    };
+    Ok((host, user.to_string(), port, db.to_string(), dsn))
 }
