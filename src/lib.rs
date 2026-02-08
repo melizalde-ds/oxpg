f2101ad5878230bf7496de3f968b9558a936cb54
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2026-02-08 02:14:45 -0700

    feat: remove unused sum_as_string function and related attributes

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,12 @@ mod client;
 
 use pyo3::prelude::*;
 use pyo3_stub_gen::define_stub_info_gatherer;
-use pyo3_stub_gen::derive::*;
 
 #[pymodule]
 fn oxpg(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(client::connect, m)?)?;
     m.add_class::<client::Client>()?;
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
-}
-
-#[gen_stub_pyfunction] // Add this
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
 }
 
 define_stub_info_gatherer!(stub_info);
